1a3041056afb8b4d7ade9f8fc0263a3f77d1124d
ytomino <demoonlit@panathenaia.halfmoon.jp>
2017-11-08 01:51:41 +0900

    Use the signature of Longident in `ocamlc -where`/compiler-libs

--- source/mpc_pp_install.ml
+++ source/mpc_pp_install.ml
@@ -1,8 +1,2 @@
-module Longident = struct
-	type t =
-		| Lident of string
-		| Ldot of t * string
-		| Lapply of t * t;;
-end;;
 open Longident;;
-Topdirs.dir_install_printer Format.std_formatter (Obj.magic (Ldot (Lident "Mpc_pp", "pp_c")));;
+Topdirs.dir_install_printer Format.std_formatter (Ldot (Lident "Mpc_pp", "pp_c"));;
--- source/mpfr_pp_install.ml
+++ source/mpfr_pp_install.ml
@@ -1,8 +1,2 @@
-module Longident = struct
-	type t =
-		| Lident of string
-		| Ldot of t * string
-		| Lapply of t * t;;
-end;;
 open Longident;;
-Topdirs.dir_install_printer Format.std_formatter (Obj.magic (Ldot (Lident "Mpfr_pp", "pp_fr")));;
+Topdirs.dir_install_printer Format.std_formatter (Ldot (Lident "Mpfr_pp", "pp_fr"));;
