3b89a56db5a87d0274e31fcf1892807e8ea0a24c
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-10-17 20:35:55 +0900

    Insert "|" into the patterns of try in examples/test_rounding.ml

--- examples/test_rounding.ml
+++ examples/test_rounding.ml
@@ -12,7 +12,8 @@ try
 	(* negative *)
 	let x = fr_of_float ~prec:8 ~mode:`A ~-.0x1.01p0 in
 	assert (x = fr_of_float ~prec:56 ~mode:`Z ~-.0x1.02p0)
-with Failure _ ->
+with
+| Failure _ ->
 	if verbose then Printf.eprintf "MPFR_RNDA is unsupported.\n";
 	let major, _, _ = mpfr_compiled_version () in
 	assert (major < 3);;
@@ -30,7 +31,8 @@ try
 	let y = FR.add ~prec:53 ~mode:`F x x in (* same as ~prec:23 ~mode:`N *)
 	let z = FR.add ~prec:23 ~mode:`N x x in
 	assert (y = z && y = fr_of_float ~prec:56 ~mode:`Z ~-.0x204p-8)
-with Failure _ ->
+with
+| Failure _ ->
 	if verbose then Printf.eprintf "MPFR_RNDF is unsupported.\n";
 	let major, _, _ = mpfr_compiled_version () in
 	assert (major < 3);;
@@ -50,7 +52,8 @@ try
 	(* zero, negative *)
 	let x = C.make_float ~prec:(8, 8) ~mode:(`A, `A) 0.0 ~-.0x1.01p0 in
 	assert (x = C.make_float ~prec:(56, 56) ~mode:(`Z, `Z) 0.0 ~-.0x1.02p0)
-with Failure _ ->
+with
+| Failure _ ->
 	if verbose then Printf.eprintf "MPC_RNDA is unsupported.\n";
 	let major, minor, patchlevel = mpc_compiled_version () in
 	let version = major lsl 16 + minor lsl 8 + patchlevel in
